a1c47f553abce1e1c39bdb5b300da4efd7c6e490
shapovalov-k <shapovalovkv@gmail.com>
2018-10-15 10:22:46 +0200

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -4,7 +4,20 @@ values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 # Создадим массив с набором мастей.
 # Буби, черви, крести, пики.
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+
+# Playing cards in Unicode
+# U+2660 - Black Spade Suit
+# U+2665 - Black Heart Suit
+# U+2666 - Black Diamond Suit
+# U+2663- Black Club Suit
+
+spade = "\u2666"
+heart = "\u2665"
+diamond = "\u2666"
+club = "\u2663"
+
+suits = [spade, heart, diamond, club]
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
 puts "#{values.sample} of #{suits.sample}"
+
